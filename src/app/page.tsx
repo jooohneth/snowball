4ac56f934845e05c62e483465320803e949fa2bb
Thai Nguyen <jooohn.eth@gmail.com>
2024-07-20 02:28:18 -0400

    feat: imports and types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import * as React from "react";
+
+import { SnowballChain } from "@snowballtools/js-sdk";
+import { useSnowball } from "@/app/snowball";
+
+import { useForm } from "react-hook-form";
+import { parseEther } from "viem";
+
+type NewUserFormData = {
+  email: string;
+};
+type VerifyEmailFormData = {
+  code: string;
+};
+type SendEthFormData = {
+  to: string;
+  amount: string;
+};
+
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
